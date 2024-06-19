50237f67ba0f685fb0b7234338c5196e4f162a9f
WinnieAtieno <winnieotieno773@gmail.com>
2024-06-19 07:54:00 +0300

    created h1 element on the dom and added text to it

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "WINNIE ATIENO is the champion";
+ 
+document.body.appendChild(newHeader)
+console.log(newHeader)
